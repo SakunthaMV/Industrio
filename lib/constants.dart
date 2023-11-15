77b8c0c7dc67eab021612da96c8c2887e6f6b745
SakunthaMV <e17340@eng.pdn.ac.lk>
2023-11-16 04:50:32 +0530

    Develop Speaker Page

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:industrio_2023/pages/people/people_page.dart';
-import 'package:industrio_2023/pages/posters/posters_page.dart';
 import 'package:industrio_2023/pages/proceeding/proceeding_page.dart';
 import 'package:industrio_2023/pages/projects/projects_page.dart';
 import 'package:industrio_2023/pages/schedule/schedule_page.dart';
@@ -22,7 +21,6 @@ const List<String> menuItems = [
   SchedulePage.route,
   PeoplesPage.route,
   ProjectPage.route,
-  PostersPage.route,
   ProceedingPage.route,
 ];
 
@@ -34,18 +32,6 @@ const List<String> ocDetails = [
   'Publicity Committee',
   'Planning Committee',
 ];
-
-// Speakers
-const Map<String, dynamic> speakersDetails = {
-  'Prof. Gamini Dissanayake': [
-    'images/speakers/speaker1.png',
-    'Emeritus Professor at University of Technology, Sydney',
-  ],
-  'Eng. Jagath Jayasundara': [
-    'images/speakers/speaker2.png',
-    'CEO of Abans Electricals PLC',
-  ],
-};
 
 // SlideShow Duration
 const int showDuration = 5000;
@@ -69,6 +55,44 @@ const String ourTheme =
     'transformation of the industry. Research projects will be presented '
     'under manufacturing engineering, manufacturing automation, '
     'logistics & supply chain, and life cycle assessment & management.';
+
+// Keynote Speaker Details
+String speakerDetails =
+    '${' ' * 10}Mr. Suneth Kotuthenne is a highly accomplished professional '
+    'in the field of manufacturing and industrial engineering with a strong '
+    'academic foundation and a wealth of practical experience. He holds '
+    'a Bachelor of Science (BSc) in Production Engineering with First '
+    'Class Honors, earned from the University of Peradeniya, and has '
+    'achieved a Master of Business Administration (MBA) with a focus '
+    'on Business Administration and Management, from the University of '
+    'Colombo. Mr. Kotuthenne has also attained qualifications in CIMA '
+    '(Chartered Institute of Management Accountants) and supply chain '
+    'management (CSCM).\n'
+    '\n${' ' * 10}Throughout his illustrious career, Mr. Kotuthenne has '
+    'contributed significantly to the field of manufacturing and industrial '
+    'engineering. He has served as a guest lecturer at both the Moratuwa '
+    'and Peradeniya Engineering faculties, where he imparts his knowledge '
+    'in the subjects of operations management and finance, nurturing the '
+    'next generation of engineering talent. Mr. Kotuthenne\'s commitment '
+    'to the academic and industrial community is further exemplified by '
+    'his involvement as a member of the Industry Advisory Board of the '
+    'Department of Manufacturing Engineering at the University of Peradeniya.\n'
+    '\n${' ' * 10}Currently, Mr. Suneth Kotuthenne serves as the Supply Chain '
+    'Director at Hemas Pharmaceuticals/Surgical & Diagnostics (Pvt) Ltd., '
+    'playing a pivotal role in optimizing supply chain operations within the '
+    'organization. Prior to his current role, Mr. Kotuthenne held the '
+    'position of Associate Director Manufacturing at Ansell, overseeing '
+    'manufacturing operations and driving strategic growth projects in the '
+    'field of supply chain in Sri Lanka and Bangladesh. Previously, he '
+    'served as Vice President Supply Chain at Analytical Instruments (Pvt) '
+    'Ltd., showcasing his expertise in supply chain management. Mr. '
+    'Kotuthenne has also contributed his valuable experience at Unilever, '
+    'holding various positions, including factory manager, adding to his '
+    'diverse portfolio in the field of manufacturing and industrial '
+    'engineering.\n'
+    '\n${' ' * 10}His extensive knowledge and leadership roles in various '
+    'organizations, along with his dedication to education and industry '
+    'collaboration, make him a valuable asset.';
 
 // Department Details
 const List<dynamic> dmieDetails = [
--- lib/pages/people/speaker_page.dart
+++ lib/pages/people/speaker_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:industrio_2023/constants.dart';
 import 'package:industrio_2023/pages/people/people_page.dart';
 import 'package:industrio_2023/pages/widgets/common_page.dart';
 
@@ -8,9 +9,70 @@ class KeynoteSpeakerPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    final double width = MediaQuery.of(context).size.width;
     return CommonPage(
       page: PeoplesPage.route,
-      content: Placeholder(),
+      content: Column(
+        children: [
+          Wrap(
+            crossAxisAlignment: WrapCrossAlignment.center,
+            children: [
+              Container(
+                height: contentHeight > width ? width * 0.8 : contentHeight,
+                width: width > 500 ? 500 : width * 0.8,
+                margin: EdgeInsets.only(top: 30.0),
+                child: Center(
+                  child: ClipOval(
+                    child: Image.asset(suneth),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: width > 500 ? 500 : width * 0.8,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FittedBox(
+                      child: Text(
+                        'Mr. Suneth Kotuthenne',
+                        style: textTheme.titleLarge?.copyWith(
+                            fontWeight: FontWeight.bold, fontSize: 50.0),
+                      ),
+                    ),
+                    FittedBox(
+                      child: Text(
+                        'Supply Chain Director at Hemas Pharmaceuticals/\nSurgical'
+                        ' & Diagnostics (Pvt) Ltd.',
+                        style: textTheme.bodyMedium?.copyWith(
+                          color: colorScheme.secondary,
+                          fontSize: 20.0,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: width > 1000 ? 1000 : width * 0.8,
+            margin: const EdgeInsets.symmetric(
+              vertical: 30.0,
+              horizontal: 20.0,
+            ),
+            child: Text(
+              speakerDetails,
+              style: textTheme.displaySmall?.copyWith(
+                fontSize: 18,
+                height: 1.5,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
